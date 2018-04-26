9504fb3b9fc66f5358215ab0623cfc5fde9fbd52
LeicaSimile <leicasimile@gmail.com>
2018-04-25 22:27:53 -0400

    Display random kitties on button click

--- app/src/main/java/com/leicasimile/dailykitty/dailykitty/MainActivity.java
+++ app/src/main/java/com/leicasimile/dailykitty/dailykitty/MainActivity.java
@@ -3,6 +3,8 @@ package com.leicasimile.dailykitty.dailykitty;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException mIOException) {
             throw new Error("Unable to update database");
         }
+
+        // Event-handlers
+        Button btnMore = findViewById(R.id.main_btnMore);
+        btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayRandomKitty();
+            }
+        });
 
         displayRandomKitty();
     }
