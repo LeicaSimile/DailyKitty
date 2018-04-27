7408e2e34e450d47ea5a344aea778d74800e7afd
LeicaSimile <leicasimile@gmail.com>
2018-04-27 19:24:47 -0400

    Add DownloadImageService

--- app/src/main/java/com/leicasimile/dailykitty/dailykitty/DownloadImageTask.java
+++ app/src/main/java/com/leicasimile/dailykitty/dailykitty/DownloadImageTask.java
@@ -1,5 +1,7 @@
 package com.leicasimile.dailykitty.dailykitty;
 
+import android.app.IntentService;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -8,6 +10,25 @@ import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DownloadImageService extends IntentService {
+    private Thread thread = new Thread();
+
+    public DownloadImageService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        thread.start();
+        ImageView bmImage = (ImageView)intent.getExtras().get("imageview");
+        String url = intent.getExtras().getString("url");
+
+        new DownloadImageTask(bmImage).execute(url);
+    }
+}
 
 // From https://stackoverflow.com/a/9288544
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
